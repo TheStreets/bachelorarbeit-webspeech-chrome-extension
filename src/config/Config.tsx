2a0b5e55daeb1bf2200f38a5c6214de583c15e2c
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-13 17:53:49 +0200

    added default export

---
+++ src/config/Config.tsx
@@ -0,0 +1,11 @@
+import "./Config.css";
+
+function Config () {
+    return (
+        <div>
+            <h1>Config works</h1>
+        </div>
+    );
+}
+
+export default Config
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,9 +1,11 @@
 import "./Home.css";
 
-export function Home() {
+function Home() {
     return (
         <div>
             <h1>Homepage works</h1>
         </div>
     );
 }
+
+export default Home
