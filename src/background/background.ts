dcdbef2046aaf69307502bac88fe0cc2bd70ad7d
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-15 02:14:39 +0200

    added functionality to communicate with the frontend

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,9 @@
+import {Message, MessageType} from "../models/Message";
+import Tab = chrome.tabs.Tab;
+
 const extensionId = chrome.i18n.getMessage("@@extension_id");
 
-function getUrl(filename: string) {
+function getUrl() {
     return "chrome-extension://" + extensionId + "/";
 }
 
@@ -11,17 +14,14 @@ function openTab(filename: any) {
         chrome.tabs.query({'windowId': win.id},
             function (tabArray) {
                 for (let i in tabArray) {
-                    console.log('Tab: ', tabArray[i] , 'Url: ', tabArray[i].url);
-                    if (tabArray[i].url === getUrl(filename)) {
+                    if (tabArray[i].url === getUrl()) {
                         // @ts-ignore
                         chrome.tabs.update(tabArray[i].id, {
                             active: true
                         });
                         tabOpen = true;
-                        console.log('Found the tab: ', tabArray[i]);
                         break;
                     }
-                    console.log('Continue the search');
                     tabOpen = false;
                 }
                 if (!tabOpen) {
@@ -36,4 +36,41 @@ function openTab(filename: any) {
 
 chrome.action.onClicked.addListener((tab) => {
     openTab("index.html")
+    const message: Message = {
+        message: "",
+        type: MessageType.START_RECOGNITION
+    };
+    setTimeout((args) => {
+        chrome.windows.getCurrent(function (win) {
+            chrome.tabs.query({'windowId': win.id},
+                function (tabArray: Tab[]) {
+                    for (let i in tabArray) {
+                        if (tabArray[i].url === getUrl()) {
+                            const connection = setupMessageConnection(tabArray[i].id as number);
+                            connection.postMessage(message);
+                            connection.onMessage.addListener(function (message, port) {
+                                handleIncomingMessages(message);
+                            });
+                            break;
+                        }
+                    }
+                }
+            );
+        });
+    }, 800);
 });
+
+
+function handleIncomingMessages(message: Message) {
+    if(message.type === MessageType.RECOGNITION_STARTED) {
+        console.log('Message received: ', message);
+    }
+}
+
+
+function setupMessageConnection(tabId:number){
+    return chrome.tabs.connect( tabId, {
+        name: extensionId
+    });
+}
+
