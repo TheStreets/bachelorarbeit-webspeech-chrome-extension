85292a6a47e5a310d5af7a09decc5d07155381da
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-19 18:55:30 +0200

    added Components Field to display the correct component in the contentScript

--- src/models/assignment.ts
+++ src/models/assignment.ts
@@ -1,4 +1,10 @@
 export interface Assignment{
     request: string;
     response: string;
+    component: Components;
+}
+
+export enum Components {
+    NO_COMPONENT,
+    INPUT_FILLER_COMPONENT
 }
