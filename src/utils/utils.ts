7cbb6990af2e8488fff1dea3df2c10a14ac521fd
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-26 01:00:32 +0200

    added response for city calls

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 // communication specific
 
-import {Weather, WeatherElement} from "../models/weather_condition";
+import {Weather, WeatherByCity, WeatherElement} from "../models/weather";
 
 export const EXTENSION_ID = chrome.i18n.getMessage("@@extension_id");
 export const CONTENT_SCRIPT_ID = `${EXTENSION_ID}/contentScript`;
@@ -17,6 +17,17 @@ export function getExtensionUrl() {
 export async function fetchWeather(lat: number, lon: number): Promise<Weather> {
     const apiKey = "63d5425e0db4c078c33bbd8253793a29";
     const url = `https://api.openweathermap.org/data/3.0/onecall?lang=de&units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Dienst zur Zeit nicht verfügbar')
+    }
+    return await response.json();
+}
+
+export async function fetchWeatherByCity(city: string): Promise<WeatherByCity> {
+    const apiKey = "63d5425e0db4c078c33bbd8253793a29";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=de&units=metric&&appid=${apiKey}`;
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -74,6 +85,27 @@ export function buildWeatherResponseForToday(weatherElements: WeatherElement[], 
             if (i === weatherElements.length){
                 // last element
                 sentence +=  'und ' + condition + ` sein. Dabei werden Temperaturen zwischen ${Math.round(minTemp)} und ${Math.round(maxTemp)} erreicht`;
+            } else {
+                sentence += condition + ', ';
+            }
+        }
+    }
+    return sentence;
+}
+
+export function buildCurrentWeatherResponseForCity(city: string, weatherElements: WeatherElement[], currentTemp): string {
+    let sentence = '';
+    if (weatherElements.length === 0) {
+        sentence = `Aktuell liegt die Temperatur in ${city} bei ${Math.round(currentTemp)} Grad Celsius`;
+    } else if (weatherElements.length === 1) {
+        sentence = `Momentan ist es in ${city} ${weatherElements[0].description} bei ${Math.round(currentTemp)} Grad Celsius`;
+    } else {
+        sentence = `Momentan ist es in ${city} `
+        for (let i = 0; i < weatherElements.length; i++) {
+            const condition = weatherElements[i].description;
+            if (i === weatherElements.length){
+                // last element
+                sentence +=  'und ' + condition + ` bei ${Math.round(currentTemp)} Grad Celsius`;
             } else {
                 sentence += condition + ', ';
             }
