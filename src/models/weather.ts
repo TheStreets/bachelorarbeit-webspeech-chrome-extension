e201365e14ce9e102a7505916a99a7e3781614bb
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-26 01:00:07 +0200

    renamed and added new interfaces for the city

--- src/models/weather.ts
+++ src/models/weather.ts
@@ -74,3 +74,50 @@ export interface Minutely {
     dt:            number;
     precipitation: number;
 }
+
+export interface WeatherByCity {
+    coord:      Coord;
+    weather:    WeatherElement[];
+    base:       string;
+    main:       Main;
+    visibility: number;
+    wind:       Wind;
+    clouds:     Clouds;
+    dt:         number;
+    sys:        Sys;
+    timezone:   number;
+    id:         number;
+    name:       string;
+    cod:        number;
+}
+
+export interface Clouds {
+    all: number;
+}
+
+export interface Coord {
+    lon: number;
+    lat: number;
+}
+
+export interface Main {
+    temp:       number;
+    feels_like: number;
+    temp_min:   number;
+    temp_max:   number;
+    pressure:   number;
+    humidity:   number;
+}
+
+export interface Sys {
+    type:    number;
+    id:      number;
+    country: string;
+    sunrise: number;
+    sunset:  number;
+}
+
+export interface Wind {
+    speed: number;
+    deg:   number;
+}
