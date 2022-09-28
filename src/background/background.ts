31585f5de709e683758416206b8de659562dc285
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-09-28 16:59:16 +0200

    added context menu: selecting text and read it out

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,12 +1,7 @@
-import {Message, MessageType} from "../models/Message";
 import {
-    BACKGROUND_ID,
-    EXTENSION_ID, getCurrentLocation,
+    EXTENSION_ID,
     getExtensionUrl
 } from "../utils/utils";
-import Tab = chrome.tabs.Tab;
-import Port = chrome.runtime.Port;
-
 
 function openTab(filename: any) {
     let tabOpen: boolean = false;
@@ -37,39 +32,46 @@ function openTab(filename: any) {
 
 chrome.action.onClicked.addListener((tab) => {
     openTab("index.html")
-    const message: Message = {
-        message: "",
-        type: MessageType.START_RECOGNITION
-    };
-    setTimeout((args) => {
-        chrome.windows.getCurrent(function (win) {
-            chrome.tabs.query({'windowId': win.id},
-                function (tabArray: Tab[]) {
-                    for (let i in tabArray) {
-                        if (tabArray[i].url === getExtensionUrl()) {
-                            // const connection = setupCommunication(tabArray[i].id as number);
-                            // connection.postMessage(message);
-                            // connection.onMessage.addListener(function (message, port) {
-                            //     handleIncomingMessages(message, port);
-                            // });
-                        }
-                    }
-                }
-            );
-        });
-    }, 800);
+    // const message: Message = {
+    //     message: "",
+    //     type: MessageType.START_RECOGNITION
+    // };
+    // setTimeout((args) => {
+    //     chrome.windows.getCurrent(function (win) {
+    //         chrome.tabs.query({'windowId': win.id},
+    //             function (tabArray: Tab[]) {
+    //                 for (let i in tabArray) {
+    //                     if (tabArray[i].url === getExtensionUrl()) {
+    //                         const connection = setupCommunication(tabArray[i].id as number);
+    //                         connection.postMessage(message);
+    //                         connection.onMessage.addListener(function (message, port) {
+    //                             handleIncomingMessages(message, port);
+    //                         });
+    //                     }
+    //                 }
+    //             }
+    //         );
+    //     });
+    // }, 800);
 });
 
-function setupCommunication(tabId: number) {
-    return chrome.tabs.connect(tabId, {
-        name: BACKGROUND_ID
+chrome.runtime.onInstalled.addListener((details) => {
+    createContextMenus();
+    chrome.contextMenus.onClicked.addListener((event) => {
+        if (event.selectionText) {
+            chrome.tts.speak(event.selectionText);
+        } else {
+            chrome.tts.speak('Bitte selektiere einen gültigen Text.');
+        }
+    });
+});
+
+function createContextMenus() {
+    chrome.contextMenus.create({
+        title: 'Text vorlesen',
+        id: `virtual_assistant/menu_1`,
+        contexts: ['selection']
     });
 }
 
-function handleIncomingMessages(message: Message, port: Port) {
-    if (message.type === MessageType.RECOGNITION_STARTED) {
-        console.log('Message received: ', message);
-    } else if (message.type === MessageType.COMMAND_CALL) {
 
-    }
-}
