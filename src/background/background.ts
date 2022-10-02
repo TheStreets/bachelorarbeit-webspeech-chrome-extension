b1893ceab7304472ca7d54f3ab976be556b9688a
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-10-02 15:15:16 +0200

    addec commucation setup between contentScript and the extension

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,10 @@
-import {
-    EXTENSION_ID,
-    getExtensionUrl
-} from "../utils/utils";
+import {EXTENSION_ID, getExtensionUrl} from "../utils/utils";
+import {Message, MessageType} from "../models/Message";
+import Port = chrome.runtime.Port;
 
+/**
+ *  helper function, open the tab, check if tab is open, then open the opened tab
+ * */
 function openTab(filename: any) {
     let tabOpen: boolean = false;
     console.log('Extension Id: ', EXTENSION_ID);
@@ -30,31 +32,16 @@ function openTab(filename: any) {
     });
 }
 
+/**
+ * listener, on click of the extension icon, open the extension a tab
+ * */
 chrome.action.onClicked.addListener((tab) => {
-    openTab("index.html")
-    // const message: Message = {
-    //     message: "",
-    //     type: MessageType.START_RECOGNITION
-    // };
-    // setTimeout((args) => {
-    //     chrome.windows.getCurrent(function (win) {
-    //         chrome.tabs.query({'windowId': win.id},
-    //             function (tabArray: Tab[]) {
-    //                 for (let i in tabArray) {
-    //                     if (tabArray[i].url === getExtensionUrl()) {
-    //                         const connection = setupCommunication(tabArray[i].id as number);
-    //                         connection.postMessage(message);
-    //                         connection.onMessage.addListener(function (message, port) {
-    //                             handleIncomingMessages(message, port);
-    //                         });
-    //                     }
-    //                 }
-    //             }
-    //         );
-    //     });
-    // }, 800);
+    openTab("index.html");
 });
 
+/**
+ * listener, that adds the context menu on the installation of the extension
+ * */
 chrome.runtime.onInstalled.addListener((details) => {
     createContextMenus();
     chrome.contextMenus.onClicked.addListener((event) => {
@@ -66,6 +53,9 @@ chrome.runtime.onInstalled.addListener((details) => {
     });
 });
 
+/**
+ * helper function, creates the context menu
+ * */
 function createContextMenus() {
     chrome.contextMenus.create({
         title: 'Text vorlesen',
@@ -74,4 +64,27 @@ function createContextMenus() {
     });
 }
 
+/**
+ * message listener for the communication between the open extension and the content script
+ * */
+chrome.runtime.onConnect.addListener(function (port: Port) {
+    port.onMessage.addListener(function (message: Message, port: Port) {
+        if (message.type === MessageType.SETUP_COMMUNICATION) {
+            console.log('Communication established');
+        } else if (message.type === MessageType.COMMAND_YOUTUBE_VIDEO_SELECTION_ON_DESKTOP) {
+            chrome.tabs.query({currentWindow: true, active: true})
+                .then(tabs => {
+                    const activeTab = tabs[0];
+                    console.log(activeTab);
+                    const isYoutubeHomepage = activeTab.url?.startsWith('https://www.youtube.com') &&
+                        !activeTab.url?.includes('/watch?v=');
+                    if (isYoutubeHomepage) {
+                        chrome.tabs.sendMessage(activeTab.id as number, message);
+                    } else {
+                        chrome.tts.speak('Diese Funktion steht nur auf der Homepage von Youtube zur Verfügung.')
+                    }
+                });
+        }
 
+    });
+});
