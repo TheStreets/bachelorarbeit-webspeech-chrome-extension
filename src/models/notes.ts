23a8b235aa17d3bd9cf6eb84ed5825df72910f3e
Eiham Salieba <eiham_salieba91@yahoo.de>
2022-11-01 22:45:08 +0100

    added fields

--- src/models/notes.ts
+++ src/models/notes.ts
@@ -2,13 +2,16 @@ export interface Note {
     text: string;
     id: number;
     date: string;
+    time: string;
 }
 
 export class Note implements Note {
     constructor(text: string, id: number) {
         this.text = text;
         this.id = id;
-        this.date = new Date().toLocaleDateString('de-DE', {day: "2-digit", month: "long", year: "numeric"});
+        const dateObject = new Date();
+        this.date = dateObject.toLocaleDateString('de-DE', {day: "2-digit", month: "long", year: "numeric"});
+        this.time = dateObject.toLocaleTimeString('de-DE', {hour: "2-digit", minute: "2-digit"});
     }
 }
 
